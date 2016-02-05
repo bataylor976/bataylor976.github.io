fcdc7e624738837beecdb5b04d2f1e6d15d473b2
Ben Taylor <bataylor976@gmail.com>
2016-02-04 23:20:05 -0500

    Added apacheDocRootReplace.py

---
+++ python/apacheDocRootReplace.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from cStringIO import StringIO
+import re
+
+# Set up three compiled regular expression objects:
+
+# One to match the start of the VirtualHost;
+vhost_start = re.compile(r'<VirtualHost\s+(.*?)>')
+# Another to match the end of the VirtualHost;
+vhost_end = re.compile(r'</VirtualHost')
+# Yet another to match the DocumentRoot line.
+docroot_re = re.compile(r'(DocumentRoot\s+)(\S+)')
+
+def replace_docroot(conf_string, vhost, new_docroot):
+    '''yield new lines of an httpd.conf file where docroot lines matching
+        the specified vhost are replaced with the new_docroot
+    '''
+    conf_file = StringIO(conf_string)
+    in_vhost = False
+    curr_vhost = None
+    for line in conf_file:
+        vhost_start_match = vhost_start.search(line)
+        if vhost_start_match:
+            curr_vhost = vhost_start_match.groups()[0]
+            in_vhost = True
+        if in_vhost and (curr_vhost == vhost):
+            docroot_match = docroot_re.search(line)
+            if docroot_match:
+                sub_line = docroot_re.sub(r'\1%s' % new_docroot, line)
+                line = sub_line
+        vhost_end_match = vhost_end.search(line)
+        if vhost_end_match:
+            in_vhost = False
+        yield line
+
+if __name__ == '__main__':
+    import sys
+    conf_file = sys.argv[1]
+    vhost = sys.argv[2]
+    docroot = sys.argv[3]
+    conf_string = open(conf_file).read()
+    for line in replace_docroot(conf_string, vhost, docroot):
+        print line,
