1587bb32e2a071da5dedba273946b2ec6d5f18c7
Taylor, Benjamin A <taylorba@ornl.gov>
2016-05-29 18:34:54 -0400

    version-check.sh rough draft complete; testing . . .

--- auto-lfs/version-check.sh
+++ auto-lfs/version-check.sh
@@ -4,7 +4,6 @@
 # While there is an Automated Linux From Scratch project, for educational purposes, I'm carrying out my own independent project.
 # Simple script to list version numbers of critical development tools
 export LC_ALL=C
-
 # Bash version check
 BASH_MINa=3
 #echo $BASH_MINa
@@ -206,6 +205,336 @@ else
 fi
 
 # Checking gcc installation information
+#This LFS version's recommended minimim for gcc is 4.7
+#This LFS version's recommended maximum for gcc is 5.3
+MYGCC=$(whereis gcc)
+if [ "$MYGCC" == "gcc:" ]
+then
+    echo "GCC not installed"
+    echo "Installing gcc . . ."
+    apt-get update
+    apt-get -y install gcc-4.8.2
+elif [ "$MYGCC" == "gcc: /usr/bin/gcc /usr/lib/gcc /usr/bin/X11/gcc /usr/share/man/man1/gcc.1.gz" ]
+then
+    MYGCCa=$(gcc --version | head -n1 | cut -d" " -f4 | cut -d"." -f1)
+    #echo "My GCCa is" $MYGCCa
+    MYGCCb=$(gcc --version | head -n1 | cut -d" " -f4 | cut -d"." -f2)
+    #echo "My GCCb is" $MYGCCb
+    if [ "$MYGCCa" -gt 5 ]
+    then
+        echo "WARNING: Installed gcc version is higher than the recommended packages. Versions higher than gcc-5.3 have not been tested with this version of LFS"
+    elif [ "$MYGCCa" -eq 5 ] && [ "$MYGCCb" -gt 3]
+    then
+        echo "WARNING: Installed gcc version is higher than the recommended packages. Versions higher than gcc-5.3 have not been tested with this version of LFS" 
+    elif [ "$MYGCCa" -eq 5 ] && [ "$MYGCCb" -eq 3 ]
+    then
+        echo "GCC version meets requirements"
+    elif [ "$MYGCCa" -eq 4 ] && [ "$MYGCCb" -ge 7 ]
+    then
+        echo "GCC version meets requirements"
+    else
+        echo "Installing latest version of gcc"
+        apt-get update
+        apt-get -y install gcc
+    fi 
+fi
+
+# Checking grep version (2.5.1a minimum)
+#grep --version | head -n1
+MYGRP=$(whereis grep)
+if [ "$MYGRP" == "grep:" ]
+then
+    echo "grep not installed"
+    echo "Installing grep . . ."
+    apt-get update
+    apt-get -y install grep
+elif [ "$MYGRP" == "grep: /bin/grep /usr/share/man/man1/grep.1.gz" ]
+then
+    MYGRPa=$(grep --version | head -n1 | cut -d" " -f4 | cut -d"." -f1)
+    MYGRPb=$(grep --version | head -n1 | cut -d" " -f4 | cut -d"." -f2)
+    if [ "$MYGRPa" -eq 2 ] && [ "$MYGRPb" -ge 5 ]
+    then
+        echo "Grep meets the minimum requirements"
+    else
+        echo "Grep does not meet the mimimum requirements"
+        echo "Installing grep . . . "
+        apt-get update
+        apt-get -y install grep
+        echo "Grep installed."
+    fi
+fi
+    
+# Checking gzip version (1.3.12 minimum)
+MYGZP=$(whereis gzip)
+if [ "$MYGZP" == "gzip:" ]
+then
+    echo "Gzip not installed"
+    echo "Installing gzip . . ."
+    apt-get update && apt-get install gzip
+elif [ "$MYGZIP" == "gzip: /bin/gzip /usr/share/man/man1/gzip.1.gz" ]
+    then
+    #MYGZPa=$(gzip --version | head -n1 | cut -d" " -f2 | cut -d"." -f1)
+    MYGZPb=$(gzip --version | head -n1 | cut -d" " -f2 | cut -d"." -f2) 
+    if [ "$MYGZIPb" -ge 3 ] 
+    then
+        echo "Gzip meets requirements"
+    else
+        echo "Gzip does not meet requirements"
+        echo "Installing latest version of gzip"
+        apt-get update
+        apt-get install gzip
+    fi
+fi   
+
+# Checking kernel version (2.6.32)
+MYKERNa=$(cat /proc/version | head -n1 | cut -d" " -f3 | cut -d"." -f1)
+MYKERNb=$(cat /proc/version | head -n1 | cut -d" " -f3 | cut -d"." -f2)
+if [ "$MYKERNa" -gt 2 ] 
+then
+    echo "Kernel version meets requirements"
+elif [ "$MYKERNa" -eq 2 ] && [ "$MYKERNb" -ge 6 ]
+then
+    echo "Kernel version meets requirements" 
+else
+    echo "Kernel does not meet requirements. Please see final messages for more details."
+fi
+
+# Checking M4 version (1.4.10 mimimum)
+MYM4=$(whereis m4)
+if [ "$MYM4" == "m4:" ]
+then
+    echo "M4 is not installed"
+    echo "Installing M4"
+    apt-get update
+    apt-get install m4
+elif [ "$MYM4" == "m4: /usr/bin/m4 /usr/bin/X11/m4 /usr/share/man/man1/m4.1.gz" ]
+    then
+    MYM4c=$(m4 --version | head -n1 | cut -d" " -f4 | cut -d "." -f3)
+    if [ "$MYM4c" -ge 10 ]
+    then
+        echo "M4 meets requirements"
+    else
+        echo "M4 does not meet requirements"
+        echo "Installing M4 . . ."
+        apt-get update 
+        apt-get install m4
+        echo "M4 installed"
+# Checking make version (3.81 minimum)
+MYMKE=$(whereis make)
+if [ "$MYMKE" == "make:" ]
+then
+    echo "Make is not installed"
+    echo "Installing make . . ."
+    apt-get update
+    apt-get install make
+elif [ "$MYMKE" == "make: /usr/bin/make /usr/bin/X11/make /usr/share/man/man1/make.1.gz" ]
+    then
+    MYMKEa=$(make --version | head -n1 | cut -d" " -f3 | cut -d"." -f1)
+    MYMKEb=$(make --version | head -n1 | cut -d" " -f3 | cut -d"." -f2)
+    if [ "$MYMKEa" -ge 3 ] && [ "$MYMKEb" -ge 81 ]
+    then
+        echo "Make meets requirements"
+    else
+        echo "Make does not meet requirements"
+        echo "Installing latest version of make . . ."
+        apt-get update
+        apt-get install make
+        echo "Make installed"
+    fi
+fi
+
+# Checking patch version (2.5.4 minimum)
+MYPTCH=$(whereis patch)
+if [ "$MYPTCH" == "patch:" ]
+then
+    echo "Patch not installed"
+    echo "Installing patch . . ."
+    apt-get update
+    apt-get install patch
+    echo "Patch installed"
+elif [ "$MYPTCH" == "patch: /usr/bin/patch /usr/bin/X11/patch /usr/share/man/man1/patch.1.gz" ]
+    then
+    MYPTCHa=$(patch --version | head -n1 | cut -d" " -f3 | cut -d"." -f1)
+    MYPTCHb=$(patch --version | head -n1 | cut -d" " -f3 | cut -d"." -f2)
+    MYPTCHc=$(patch --version | head -n1 | cut -d" " -f3 | cut -d"." -f3)
+    if [ "$MYPTCHa" -eq 2 ] && [ "$MYPTCHb" -gt 5 ]
+    then
+        echo "Patch meets requirements"
+    elif [ "$MYPTCHa" -eq 2 ] && [ "$MYPTCHb" -eq 5 ] && [ "$MYPTCHc" -ge 4 ]
+    then
+        echo "Patch meets requirements"
+    else
+        echo "Patch does not meet requirements"
+        echo "Installing latest version of patch . . ."
+        apt-get update
+        apt-get install patch
+    fi
+fi
+
+
+# Checking perl version (5.8.8 minimum)
+MYPRL=$(whereis perl)
+if [ "$MYPRL" == "perl:" ]
+then
+    echo "Perl is not installed"
+    echo "Installing perl . . ."
+    apt-get update
+    apt-get install perl
+elif [ "$MYPRL" == "perl: /usr/bin/perl /etc/perl /usr/lib/perl /usr/bin/X11/perl /usr/share/perl /usr/share/man/man1/perl.1.gz" ]
+    then
+    MYPRLa=$(echo Perl `perl -V:version` | cut -d"'" -f2 | cut -d"." -f1)
+    MYPRLb=$(echo Perl `perl -V:version` | cut -d"'" -f2 | cut -d"." -f2)
+    MYPRLc=$(echo Perl `perl -V:version` | cut -d"'" -f2 | cut -d"." -f3)
+    if [ "$MYPRLa" -ge 5 ] && [ "$MYPRLb" -ge 8 ] && [ "$MYPRLb" -ge 8 ]
+    then
+        echo "Perl meets requirements"
+    else
+        echo "Perl does not meet requirements"
+        echo "Installing latest version of perl"
+        apt-get update
+        apt-get install perl
+    fi
+fi
+
+# Checking sed version (4.1.5 minimum)
+MYSED=$(whereis sed)
+if [ "$MYSED" == "sed:" ]
+then
+    echo "Sed is not installed"
+    echo "Installing sed. . ."
+    apt-get update
+    apt-get intall sed
+    echo "Sed installed"
+elif [ "$MYSED" == "sed: /bin/sed /usr/share/man/man1/sed.1.gz" ]
+then
+    MYSEDa=$(sed --version | head -n1 | cut -d" " -f4 | cut -d"." -f1)
+    MYSEDb=$(sed --version | head -n1 | cut -d" " -f4 | cut -d"." -f2)
+    MYSEDc=$(sed --version | head -n1 | cut -d" " -f4 | cut -d"." -f3)
+    # Most likely not going to have any version lower than 4
+    if [ "$MYSEDb" -ge 1 ] && [ "$MYSEDc" ge 5 ]
+    then
+        echo "Sed meets requirements"
+    else
+        echo "Sed does not meet requirements"
+        echo "Installing sed . . ."
+        apt-get update
+        apt-get install sed
+        echo "Sed installed"
+    fi
+fi
+    
+# Checking tar version (1.22 minimum)
+MYTAR=$(whereis tar)
+if [ "$MYTAR" == "tar:" ]
+then
+    echo "Tar is not installed"
+    echo "Installing tar. . ."
+    apt-get update
+    apt-get intall tar
+    echo "Tar installed"
+elif [ "$MYTAR" == "tar: /bin/tar /usr/lib/tar /usr/include/tar.h /usr/share/man/man1/tar.1.gz" ]
+then
+    MYTARa=$(tar --version | head -n1 | cut -d" " -f4 | cut -d"." -f1)
+    MYTARb=$(tar --version | head -n1 | cut -d" " -f4 | cut -d"." -f2)
+    if [ "$MYTARa" -ge 1 ] && [ "$MYTARb" -ge 22 ]
+    then
+        echo "Tar meets requirements"
+    else
+        echo "Tar does not meet requirements"
+        echo "Installing tar . . ."
+        apt-get update
+        apt-get install tar
+        echo "Tar installed"
+    fi
+fi
+
+# Checking makeinfo version (4.7 minimum)
+MYMKNFO=$(whereis makeinfo)
+if [ "$MYMKNFO" == "makeinfo:" ]
+then
+    echo "Makeinfo is not installed"
+    echo "Installing makeinfo. . ."
+    apt-get update
+    apt-get -y install texinfo
+    echo "Makeinfo installed"
+elif [ "$MYMKNFO" == "makeinfo: /usr/bin/makeinfo /usr/bin/X11/makeinfo /usr/share/man/man1/makeinfo.1.gz" ]
+then
+    MYMKNFOa=$(makeinfo --version | head -n1 | cut -d" " -f4 | cut -d"." -f1)
+    MYMKNFOb=$(makeinfo --version | head -n1 | cut -d" " -f4 | cut -d"." -f2)
+    if [ "$MYMKNFOa" -gt 4 ] 
+    then
+        echo "Makeinfo meets requirements"
+    elif [ "$MYMKNFOa" -eq 4 ] && [ "$MYMKNFOb" -ge 7 ]
+    then
+        echo "Makeinfo meets requirements"
+    else
+        echo "Makeinfo does not meet requirements"
+        echo "Installing makeinfo . . ."
+        apt-get update
+        apt-get install texinfo
+        echo "Makeinfo installed"
+    fi
+fi
+
+# Checking xz version
+
+#xz --version | head -n1
+MYXZ=$(whereis xz)
+if [ "$MYXZ" == "xz:" ]
+then
+    echo "Xz is not installed"
+    echo "Installing xz. . ."
+    apt-get update
+    apt-get -y install xz-utils
+    echo "Xz installed"
+elif [ "$MYXZ" == "xz: /usr/bin/xz /usr/bin/X11/xz /usr/share/man/man1/xz.1.gz" ]
+then
+    MYXZa=$(xz --version | head -n1 | cut -d" " -f4 | cut -d"." -f1)
+    MYXZb=$(xz --version | head -n1 | cut -d" " -f4 | cut -d"." -f2)
+    if [ "$MYXZa" -eq 5 ] && [ "$MYXZb" -ge 0 ]
+    then
+        echo "Xz meets requirements"
+    else
+        echo "Xz does not meet requirements"
+        echo "Installing xz-utils . . ."
+        apt-get update
+        apt-get install xz-utils
+        echo "Xz installed"
+    fi
+fi
+
+# Final sanity check list (including test run of natively install g++ and ldd)
+echo "Running final sanity check for software prerequisites"
+bash --version | head -n1 | cut -d" " -f2-4
+MYSH=$(readlink -f /bin/sh)
+echo "/bin/sh -> $MYSH"
+echo $MYSH | grep -q bash || echo "ERROR: /bin/sh does not point to bash"
+unset MYSH
+
+echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
+bison --version | head -n1
+
+if [ -h /usr/bin/yacc ]; then
+  echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
+elif [ -x /usr/bin/yacc ]; then
+  echo yacc is `/usr/bin/yacc --version | head -n1`
+else
+  echo "yacc not found" 
+fi
+
+bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-
+echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
+diff --version | head -n1
+find --version | head -n1
+gawk --version | head -n1
+
+if [ -h /usr/bin/awk ]; then
+  echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`";
+elif [ -x /usr/bin/awk ]; then
+  echo awk is `/usr/bin/awk --version | head -n1`
+else 
+  echo "awk not found" 
+fi
 
 gcc --version | head -n1
 g++ --version | head -n1
